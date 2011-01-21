1201c0e0ae242f6a36bc80327ee5e961b8322f02
N3ro <venezuela.gtug@gmail.com>
2011-01-21 00:05:33 -0430

    project

---
+++ src/com/git/nerosatanas/andpaste/AndPaste.java
@@ -0,0 +1,13 @@
+package com.git.nerosatanas.andpaste;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AndPaste extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
